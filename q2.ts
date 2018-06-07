aaf2aa9072fc81d06dab6d35c8d84c89caed8cdc
abadied <abadi.eden@gmail.com>
2018-06-07 15:04:32 +0300

    started task2

--- q2.ts
+++ q2.ts
@@ -26,8 +26,23 @@ export const t3: Tree = {children: [
 
 
 export const leftMostEven1 = (atree: Tree): number => {
-  // your code here
-  return 0;
+  if(isTreeLeaf(atree)){
+    if(atree.value % 2 === 0)
+      return atree.value;
+    else
+      return -1;
+  }
+  else{
+    if(atree.children.length === 0)
+      return -1;
+    for(let i = 0; i < atree.children.length - 1; i++){
+      let ans_car = leftMostEven1(atree.children[i]);
+      if(ans_car != -1)
+        return ans_car;
+      else 
+        return leftMostEven1(atree.children[i + 1])
+    }
+  }
 }
 
 
@@ -41,7 +56,8 @@ export const leftMostEven2 = (atree: Tree): number =>
 const leftMostEven$ = <T1, T2>(atree: Tree,
                                succ: ((x:number) => T1),
                                fail: (() => T2)): (T1 | T2) =>
-    // your code here
+    isTreeLeaf(atree) ? (atree.value % 2 === 0) ? succ(atree.value) : fail() :
+    isTreeNode(atree) ? leftMostEven$({children: atree.children.slice(0, 1)}, succ, fail) :
     fail();
 
 
